fa23ca99bf578def5d7e6be642a9191cd7b67a0e
MassimoRedomi <redomisenior@icloud.com>
2023-12-07 18:57:11 +0100

    add spring/express/python files

---
+++ solution/secondary_spring/src/main/java/com/iumtweb/secondary_spring/SecondarySpringApplication.java
@@ -0,0 +1,13 @@
+package com.iumtweb.secondary_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecondarySpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecondarySpringApplication.class, args);
+	}
+
+}
---
+++ solution/secondary_spring/src/test/java/com/iumtweb/secondary_spring/SecondarySpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iumtweb.secondary_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecondarySpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
