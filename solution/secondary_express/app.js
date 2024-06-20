1d79a09c23afbf8a471614af301dc039e97a7220
MassimoRedomi <redomisenior@icloud.com>
2024-06-20 17:42:18 +0200

    idk

--- solution/secondary_express/app.js
+++ solution/secondary_express/app.js
@@ -33,14 +33,12 @@ app.use('/api', gameEventsRouter);
 app.use('/api', careerRoutes);
 app.use('/api', rankingsRoutes);
 
-// catch 404 and forward to error handler
+
 app.use(function(req, res, next) {
   next(createError(404));
 });
 
-// error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
